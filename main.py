3c636092a77b642622576d3f2afc380bbb3a4b27
averdonks <62974106+averdonks@users.noreply.github.com>
2025-06-04 08:23:52 -0400

    Add functionality to get the number of words from a text file

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+def get_num_words(book_text):
+    word_list = book_text.split()
+    return len(word_list)
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    print(f"{get_num_words(book_text)} words found in the document")
 
 main()
