fbdc44a30e63064e4f2db0392a1e19fd65c99b26
averdonks <62974106+averdonks@users.noreply.github.com>
2025-06-05 07:35:40 -0400

    Refactor code by adding stats.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-
-def get_num_words(book_text):
-    word_list = book_text.split()
-    return len(word_list)
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book_text):
+    word_list = book_text.split()
+    return len(word_list)
