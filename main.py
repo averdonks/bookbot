8485012d1ea41efedb2a21c1625bfc5a4d0c9189
averdonks <62974106+averdonks@users.noreply.github.com>
2025-06-13 18:48:38 -0400

    Add code to only allow the user input .txt files

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import sys
 
+from pathlib import Path
+
 from stats import (
   get_num_words,
   get_characters_dict,
   characters_dict_to_sorted_list  
 )
 
+# Checks if the user entered two arguments
 if len(sys.argv) != 2:
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
+# Checks if the user entered a .txt file into the second argument
+if Path(sys.argv[1]).suffix != ".txt":
+    print("Error: The book must be a .txt file")
+    sys.exit(1)
 
+# Opens and reads the book text
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+# Prints a report 
 def print_report(path_to_book, num_words, characters_sorted_list):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_book}...")
